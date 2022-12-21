fdb0235b49ba906b9ee20ed5b28ead6c05e225f3
Amoljamnik97 <ajamnik.mech@gmail.com>
2022-12-21 18:09:56 +0530

    [amol]- EnterPassword First should be Cap

--- RegexProblems/RegexProblems/Program.cs
+++ RegexProblems/RegexProblems/Program.cs
@@ -5,9 +5,9 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Enter Passwored Minimum 8");
+        Console.WriteLine("Enter Passwored Minimum");
         var data = Console.ReadLine();
-        string pattern = "^[0-9A-Za-z]{8,}$";
+        string pattern = "^[A-Z]{1}[0-9A-Za-z]{8,}$";
         Regex regex = new Regex(pattern);
 
         if(regex.IsMatch(data))
