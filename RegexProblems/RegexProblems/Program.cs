83a5fc2f7fbdd9d8dbbfb407c64dc5237d6fb6b6
Amoljamnik97 <ajamnik.mech@gmail.com>
2022-12-21 16:57:37 +0530

    [amol]- Enter First Name

--- RegexProblems/RegexProblems/Program.cs
+++ RegexProblems/RegexProblems/Program.cs
@@ -1,2 +1,26 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter Name");
+        var data = Console.ReadLine();
+        string pattern = "[A-Z]{1}[a-z]{2,}";
+        Regex regex = new Regex(pattern);
+
+        if(regex.IsMatch(data))
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(data + " is Valid");
+            Console.ResetColor();
+        }
+        else
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(data + " is Not Valid");
+            Console.ResetColor();
+        }
+    }
+}
